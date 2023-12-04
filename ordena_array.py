3af886cdb1dc1fb6aa92f00359d268e7ca1d11b5
boniiilla <c.bonillac00@gmail.com>
2023-12-04 13:35:38 +0100

    Modificado bueno

--- ordena_array.py
+++ ordena_array.py
@@ -6,4 +6,21 @@ array = [46, 50, 65, 97, 69, 33, 59, 71, 98, 47, 55, 82, 35,  9, 75, 39, 93,
        89, 86, 68, 88, 14,  3, 30, 58, 80, 31, 37, 83, 24,  2, 26]
 
 size_array = len(array)
+contador_posicion = 0
+save_array_i = 0
+'''
+Queremos comparar la primera posicion las otras.
+Si posicion 1 es mayor que posicion 2, cambiamos el valor de posicion 1 por 2 y viceversa.
+Asi con todas las cadenas hasta llegar a la ultima.
+'''
 
+for i in range(0,size_array):
+    for j in range(contador_posicion,size_array):
+       if array[i] > array[j]:
+           save_array_i = array[i]
+           array[i] = array[j]
+           array[j] = save_array_i
+    contador_posicion += 1
+
+
+print(array)
