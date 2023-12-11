aa20a8eadc0f98245d9aada1d15e876ffe37703d
boniiilla <c.bonillac00@gmail.com>
2023-12-11 13:04:33 +0100

    Nuevo

---
+++ matrius.py
@@ -0,0 +1,16 @@
+matriu = [[1, 2, 3], [3, 8], [1, 8, 7]]
+
+print("Matriu inicial")
+for fila in matriu:
+    for element in fila:
+        print(element, end=" ")
+    print()
+
+#TODO
+
+
+print("Matriu final:")
+for fila in matriu:
+    for element in fila:
+        print(element, end=" ")
+    print()
