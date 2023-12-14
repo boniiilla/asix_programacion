475a612bb49f037a25a8a1a0ffbfb7dae1908a6e
boniiilla <c.bonillac00@gmail.com>
2023-12-14 13:45:12 +0100

    modificado

--- pruebas.py
+++ pruebas.py
@@ -1,7 +1,21 @@
 x = [1,2,4,7]
+
+print("COPIA X a Y")
+y = x.copy()
+y[1] = 6
+for i in range(len(x)):
+    print(x[i])
+
+print("AÑADIR VALORES DE X a Y")
+y = []
+for value in x:
+    y.append(value)
+y[1] = 6
+for i in range(len(x)):
+    print(x[i])
+
+print("ACCEDER A X DES DE Y")
 y = x
 y[1] = 6
-
-# y hace referencia a x
 for i in range(len(x)):
     print(x[i])
