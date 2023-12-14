18b4ae9805b0118f697066aecdadb7f8eceba290
boniiilla <c.bonillac00@gmail.com>
2023-12-14 14:11:14 +0100

    14/12 a las 14.11

--- copias.py
+++ copias.py
@@ -1,8 +1,8 @@
-x = [1,2,4,7]
+x = [[1,2],[4,7]]
 
 print("COPIA X a Y")
 y = x.copy()
-y[1] = 6
+y[0][1] = 6
 for i in range(len(x)):
     print(x[i])
 
@@ -10,12 +10,12 @@ print("AÑADIR VALORES DE X a Y")
 y = []
 for value in x:
     y.append(value)
-y[1] = 6
+y[0][1] = 6
 for i in range(len(x)):
     print(x[i])
 
 print("ACCEDER A X DES DE Y")
 y = x
-y[1] = 6
+y[0][1] = 6
 for i in range(len(x)):
     print(x[i])
