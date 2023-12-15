be98f33432ce1654b37c7ec32e21fb7d06f68b76
boniiilla <c.bonillac00@gmail.com>
2023-12-15 09:01:52 +0100

    Acabamos el codigo para añadir estudiantes en forma de tupla

--- estudiantes.py
+++ estudiantes.py
@@ -1,19 +1,22 @@
-#PRE: Añadim una llista de usuaris amb la següent informació: Nom, Cognom, Edat, Nota1, Nota2, Nota3
+#PRE: Añadim una llista de usuaris amb la següent informació: Nom, Cognom, Edat, Nota1, Nota2, Nota3 (Notas con valor entre 0-10)
 estudiantes = []
 
-nombre_estudiants = int(input("Introdueix el nombre de estudiants a afegir:"))
+nombre_estudiants = int(input("Introdueix el nombre de estudiants a afegir: "))
 estudiants_afegits = 0
-estudiante = []
 while estudiants_afegits < nombre_estudiants:
+    estudiante = []
     nom = str(input("Nom: "))
     cognom = str(input("Cognom: "))
     edat = int(input("Edat: "))
     nota1 = int(input("Nota 1: "))
     nota2 = int(input("Nota 2: "))
-    nota3 = int(input(f"Nota 3: "))
-    estudiante.append(nom,cognom,edat,nota1,nota2,nota3)
-    estudiantes.append(estudiante)
+    nota3 = int(input("Nota 3: "))
+    estudiante.append(nom)
+    estudiante.append(cognom)
+    estudiante.append(edat)
+    estudiante.append(nota1)
+    estudiante.append(nota2)
+    estudiante.append(nota3)
+    estudiantes.append(tuple(estudiante))
     estudiants_afegits += 1
-
-print(estudiante)
-print(estudiantes)
+    print(estudiantes)
