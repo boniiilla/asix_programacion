97d09427d4d5e0a6eb6778a71b37ee6236220df9
boniiilla <c.bonillac00@gmail.com>
2023-12-15 09:24:16 +0100

    Descomentamos para hacer las pruebas necesarias y empezar a trabajar con las notas de los alumnos.

--- estudiantes.py
+++ estudiantes.py
@@ -1,26 +1,35 @@
 #PRE: Añadim una llista de usuaris amb la següent informació: Nom, Cognom, Edat, Nota1, Nota2, Nota3 (Notas con valor entre 0-10)
 estudiantes = [("Carlos", "Bonilla", 18, 7, 6, 10), ("Ruben", "Rodriguez", 20, 6, 5, 9), ("Izan", "Lozano", 18, 1, 5, 8)]
-"""
-CODIGO PARA AÑADIR ESTUDIANTES AUTOMATICAMENTE:
+
+#CODIGO PARA AÑADIR ESTUDIANTES AUTOMATICAMENTE:
 
 nombre_estudiants = int(input("Introdueix el nombre de estudiants a afegir: "))
 estudiants_afegits = 0
+
 while estudiants_afegits < nombre_estudiants:
-    estudiante = []
+    estudiante = [] #Limpiar los datos del estudiante
+
+    #Asignamos la informacion en variables del estudiante
     nom = str(input("Nom: "))
     cognom = str(input("Cognom: "))
     edat = int(input("Edat: "))
     nota1 = float(input("Nota 1: "))
     nota2 = float(input("Nota 2: "))
     nota3 = float(input("Nota 3: "))
+
+    #Añadimos a la lista estudiante
     estudiante.append(nom)
     estudiante.append(cognom)
     estudiante.append(edat)
     estudiante.append(nota1)
     estudiante.append(nota2)
     estudiante.append(nota3)
-    estudiantes.append(tuple(estudiante))
+
+    estudiantes.append(tuple(estudiante)) #Añadimos la lista en forma de TUPLA a los estudantes
+
     estudiants_afegits += 1
-    print(estudiantes)
-"""
+
+
+
+print(estudiantes)
 
--- pruebas.py
+++ pruebas.py
@@ -1,3 +1,4 @@
 estudiantes = [("Carlos", "Bonilla", 18, 7, 6, 10), ("Ruben", "Rodriguez", 20, 6, 5, 9), ("Izan", "Lozano", 18, 1, 5, 8)]
 
-print(estudiantes[0][0])
+print(estudiantes[0])
+
