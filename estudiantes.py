7aed0c956f090455246818f58c37ffa3bc03a418
boniiilla <c.bonillac00@gmail.com>
2023-12-15 10:06:51 +0100

    He añadido el codigo para los alumnos que cumplen la condicion 1 y la condicion 2.

--- estudiantes.py
+++ estudiantes.py
@@ -1,6 +1,8 @@
 #PRE: Añadimos una lista de usuarios con la siguiente inforación: Nom, Cognom, Edat, Nota1, Nota2, Nota3 (Notas con valor entre 0-10)
 estudiantes = [("Carlos", "Bonilla", 18, 7, 6, 10), ("Ruben", "Rodriguez", 20, 6, 5, 9), ("Izan", "Lozano", 18, 1, 5, 8)]
-
+estudiantes_condicion1 = []
+estudiantes_condicion2 = []
+"""
 #CODIGO PARA AÑADIR ESTUDIANTES AUTOMATICAMENTE:
 
 nombre_estudiants = int(input("Introdueix el nombre de estudiants a afegir: "))
@@ -9,7 +11,7 @@ estudiants_afegits = 0
 
 while estudiants_afegits < nombre_estudiants:
     estudiante = [] #Limpiar los datos del estudiante
-    notas_estudiante = int(input("Introdueix el nombre de notes del estudiant: ")) #Numero de notas del estudiante
+    notas_estudiante = 3
     #Asignamos la informacion en variables del estudiante
     nom = str(input("Nom: "))
     cognom = str(input("Cognom: "))
@@ -27,8 +29,21 @@ while estudiants_afegits < nombre_estudiants:
     estudiantes.append(tuple(estudiante)) #Añadimos la lista en forma de TUPLA a los estudantes
 
     estudiants_afegits += 1
+"""
+
+for estudiante in range(len(estudiantes)):
+    nota_media = (estudiantes[estudiante][3]*0.3) + (estudiantes[estudiante][4]*0.4) + (estudiantes[estudiante][5]*0.3)
+    if nota_media > 7:
+        estudiantes_condicion1.append(estudiantes[estudiante])
+    print(nota_media)
+    
+    for nota in range(3,6):
+        if estudiantes[estudiante][nota] > 8:
+            estudiantes_condicion2.append(estudiantes[estudiante])
+            break
 
 
-
+print(estudiantes_condicion1)
+print(estudiantes_condicion2)
 print(estudiantes)
 
