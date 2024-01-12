608c9c905e43327d9e126eb4f82574be7e2fa6c8
boniiilla <c.bonillac00@gmail.com>
2024-01-12 09:23:55 +0100

    Codigo completo

--- llista_aleatori.py
+++ llista_aleatori.py
@@ -1,15 +1,23 @@
 # PRE: Definim dues llistes, una de 10 nombres i un altre de entre 5 i 8.
 
-lista_10_nombres = []
-lista_8_nombres = []
+lista_1 = []
+lista_2 = []
 
 seed = int(input("Introduce un numero cualquiera: "))
 
-for i in range(10):
+for i in range(1):
     seed = (seed * 997) % 1000
     random = (seed * 503) % 1000 / 1000
-    numero = int((random*100) // 10)
-    lista_10_nombres.append(numero)
+    numero_aleatorio = int((random * 100))
+    print(numero_aleatorio)
+
+print("LISTA 1:")
+for i in range(numero_aleatorio):
+    seed = (seed * 997) % 1000
+    random = (seed * 503) % 1000 / 1000
+    numero = int(random*10)
+    lista_1.append(numero)
+    print("\t",numero)
 
 seed = int(input("Vuelve a introducir un numero cualquiera: "))
 
@@ -17,14 +25,29 @@ for i in range(1):
     seed = (seed * 997) % 1000
     random = (seed * 503) % 400 / 100
     numero_aleatorio = int((random // 1) + 5)
-    
+
+print("LISTA 2:")
 for i in range(numero_aleatorio):
     seed = (seed * 997) % 1000
     random = (seed * 503) % 1000 / 1000
-    numero = int((random*100) // 10)
-    lista_8_nombres.append(numero)
+    numero = int(random*10)
+    lista_2.append(numero)
+    print("\t",numero)
+
+lista_completa = lista_1 + lista_2
+
+lista_sin_duplicados = []
+suma = 0
+
+for i in lista_completa:
+    if i not in lista_sin_duplicados:
+        lista_sin_duplicados.append(i)
+        suma = suma + i
+    
+print("Suma de tots els nonbres:", suma)
+
+# POST: Mostra les dues llistes i la suma final.
 
 
 
-
-#print(lista_10_nombres)
+#print(lista_1)
