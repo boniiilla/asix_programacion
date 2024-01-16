d8aa4d8aab688b10b6b266f9dc86397e808fd80d
boniiilla <c.bonillac00@gmail.com>
2024-01-16 10:37:45 +0100

    Codigo empezado anteriormente para sacar

--- cors_nuevo.py
+++ cors_nuevo.py
@@ -14,5 +14,35 @@ for j in range (13):
         random = (seed * 503) % 1000 / 1000
         carta = int(random * (len(cartas)))
         jugadores[i].append(cartas.pop(carta))
-        
+
+'''
+for jugador in jugadores:
+    print(f"Jugador: {index} ",','.join(jugador))
+    print("Cartas repartidas a este jugadro:", len(jugador))
+    index = index + 1
+'''
+
+# Que toca hacer ahora?
+
+'''
+print(jugadores[0][8][-1]) # Sacamos el PALO de la carta
+print(jugadores[0][8][0:-1]) # Sacamos el Numero de la carta
+'''
+
+cartas_tiradas = [[], [], [], []] # Cada jugador tirara su propia carta y las compararemos (cartas_tiradas[0] -> carta de jugador[0] = jugador1)
+
+aux_palo_alto = [[], [], [], []]
+aux_numero_alto = [[], [], [], []]
+
+i = 0
+while i < 13:
+    j = 0
+    cartas_tiradas = [[], [], [], []]
+    while j < 4:
+        cartas_tiradas[j].append(jugadores[j].pop())
+        j = j + 1
+    
+    print(cartas_tiradas)
+    i = i + 1
+    
 print(jugadores)
