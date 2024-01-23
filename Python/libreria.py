54d71b2ced49aeb9bcecd843160295c7251a2e45
boniiilla <c.bonillac00@gmail.com>
2024-01-23 10:58:56 +0100

    Error en la linea 33 index out of range

--- Python/libreria.py
+++ Python/libreria.py
@@ -1,20 +1,35 @@
 libros = ["Harry Potter y la Piedra Filosofal", "Harry Potter y la Camara de los Secretos", "Harry Potter y el Priosionero de Azkaban", "Los Pitufos", "Doraemon"]
-
+libros_prestados = []
 accion = 'x'
 
 while accion.upper() != 'S':
-    accion = input("INTRODUCE LA OPCION: 'M' Mostrar Libreria | 'A' Añadir Libro | 'E' Eliminar Libro | 'S' Salir :  ")
+    accion = input('''
+OPCIONES:
+    'M': Mostrar libreria y libros prestados
+    'A': Añadir libro
+    'E': Eliminar libro
+    'P': Prestar libro
+    'D': Devolver libro
+    'S': Salir
+
+INTRODUCE OPCION: ''')
     accion = accion.upper()
     if accion == 'M':
-        print(libros)
-    else:
-        if accion == 'A':
-            libro_nuevo = input("Libro que desea añadir: ")
-            libros.append(libro_nuevo)
-        else:
-            if accion == 'E':
-                libro_eliminado = input("Libro que desea eliminar: ")
-                libros.remove(libro_eliminado)
-            else:
-                if accion != 'S':
-                    print("OPCION ERRONEA")
+        print("LIBRERIA:")
+        for libro in libros:
+            print(libro)
+        print("\nPRESTADOS:")
+        for prestado in libros_prestados:
+            print(prestado)
+    elif accion == 'A':
+        libro_nuevo = input("Libro que desea añadir: ")
+        libros.append(libro_nuevo)
+    elif accion == 'E':
+        libro_eliminado = input("Libro que desea eliminar: ")
+        libros.remove(libro_eliminado)
+    elif accion == 'P':
+        libro_prestado = input("Libro que desea coger como prestamo: ")
+        for i in range(len(libros)):
+            print(libros[i])
+            if libros[i] == libro_prestado:
+                libros_prestados.append(libros.pop(i))
