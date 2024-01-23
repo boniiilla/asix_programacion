713e70d62aab3c89cb5e847e051aaaa7046d2efd
boniiilla <c.bonillac00@gmail.com>
2024-01-23 10:25:27 +0100

    Se me habia olvidado el error

--- Python/libreria.py
+++ Python/libreria.py
@@ -1,15 +1,17 @@
 libros = []
 
 accion = 0
-while accion != 'S':
-    accion = input("INTRODUCE LA SIGUIENTE ACCION: 'M' Mostrar Libreria | 'A' Añadir Libro | 'E' Eliminar Libro | 'S' Salir :  ")
-    if accion == 'M':
+while accion.upper() != 'S':
+    accion = input("INTRODUCE LA OPCION: 'M' Mostrar Libreria | 'A' Añadir Libro | 'E' Eliminar Libro | 'S' Salir :  ")
+    if accion.upper() == 'M':
         print(libros)
     else:
-        if accion == 'A':
+        if accion.upper() == 'A':
             libro_nuevo = input("Libro que desea añadir: ")
             libros.append(libro_nuevo)
         else:
-            if accion == 'E':
+            if accion.upper() == 'E':
                 libro_eliminado = input("Libro que desea eliminar: ")
                 libros.remove(libro_eliminado)
+            else:
+            print("OPCION ERRONEA")
