bde434dcf6d2518896488a5148f44862ea2e15ab
boniiilla <c.bonillac00@gmail.com>
2024-01-23 10:37:43 +0100

    Ultimos retoques

--- Python/libreria.py
+++ Python/libreria.py
@@ -1,17 +1,20 @@
-libros = []
+libros = ["Harry Potter y la Piedra Filosofal", "Harry Potter y la Camara de los Secretos", "Harry Potter y el Priosionero de Azkaban", "Los Pitufos", "Doraemon"]
 
-accion = 0
+accion = 'x'
+
 while accion.upper() != 'S':
     accion = input("INTRODUCE LA OPCION: 'M' Mostrar Libreria | 'A' Añadir Libro | 'E' Eliminar Libro | 'S' Salir :  ")
-    if accion.upper() == 'M':
+    accion = accion.upper()
+    if accion == 'M':
         print(libros)
     else:
-        if accion.upper() == 'A':
+        if accion == 'A':
             libro_nuevo = input("Libro que desea añadir: ")
             libros.append(libro_nuevo)
         else:
-            if accion.upper() == 'E':
+            if accion == 'E':
                 libro_eliminado = input("Libro que desea eliminar: ")
                 libros.remove(libro_eliminado)
             else:
-            print("OPCION ERRONEA")
+                if accion != 'S':
+                    print("OPCION ERRONEA")
