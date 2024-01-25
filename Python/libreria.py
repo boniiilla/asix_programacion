b2de966344c055d7ab698354476b2c7dc96d5569
boniiilla <c.bonillac00@gmail.com>
2024-01-25 08:50:20 +0100

    \n añadidas

--- Python/libreria.py
+++ Python/libreria.py
@@ -22,13 +22,13 @@ INTRODUCE OPCION: ''')
         for prestado in libros_prestados:
             print(prestado)
     elif accion == 'A':
-        libro_nuevo = input("Libro que desea añadir: ")
+        libro_nuevo = input("\nLibro que desea añadir: ")
         libros.append(libro_nuevo)
     elif accion == 'E':
-        libro_eliminado = input("Libro que desea eliminar: ")
+        libro_eliminado = input("\nLibro que desea eliminar: ")
         libros.remove(libro_eliminado)
     elif accion == 'P':
-        libro_prestado = input("Libro que desea coger como prestamo: ")
+        libro_prestado = input("\nLibro que desea coger como prestamo: ")
         prestado_bool = False
         i = 0
         while i < len(libros) and prestado_bool == False:
@@ -39,11 +39,11 @@ INTRODUCE OPCION: ''')
             if i == (len(libros) - 1):
                 print("\nEl libro no esta en la libreria.")
     elif accion == 'D':
-        libro_devuelto = input("Libro que desea devolver: ")
+        libro_devuelto = input("\nLibro que desea devolver: ")
         for prestado in range(len(libros_prestados)):
             if libros_prestados[prestado] == libro_devuelto:
                 libros.append(libros_prestados.pop(prestado))
             else:
-                print("Libro no encontrado en prestamos.")
+                print("\nLibro no encontrado en prestamos.")
     else:
         print("\nOpcion Erronea!")
