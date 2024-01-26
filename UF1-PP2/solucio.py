a2ffb6f167ff0eb94139fde618bd79f1aac9e89a
boniiilla <c.bonillac00@gmail.com>
2024-01-26 09:39:22 +0100

    si

--- UF1-PP2/solucio.py
+++ UF1-PP2/solucio.py
@@ -2,12 +2,12 @@
 puntuacions = [[13, 19, 17, 11, 5, 14, 14, 1, 1, 16, 5, 16, 14, 7, 17], [10, 16, 1, 18, 17, 17, 14, 20, 10, 7, 7, 8, 2, 10, 9], [10, 18, 11, 4, 2, 7, 6, 2, 4, 15, 11, 9, 6, 12, 17], [11, 10, 11, 13, 10, 16, 5, 7, 9, 11, 18, 5, 1, 4, 1], [2, 12, 16, 5, 12, 18, 7, 11, 7, 18, 7, 6, 6, 18, 5]]
 mitjana = 0
 categories = {
-    "Categories A" : [],
-    "Categories B" : [],
-    "Categories C" : [],
-    "Categories D" : [],
-    "Categories E" : [] } # [[Categoria A],[Categoria B],[Categoria C],[Categoria D],[Categoria E]]
-patrons_temporals_avancats = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]] #patrons_temporals_avançats > jugador > pics_max, pics_min
+    "Categoria A" : [],
+    "Categoria B" : [],
+    "Categoria C" : [],
+    "Categoria D" : [],
+    "Categoria E" : [] } # [[Categoria A],[Categoria B],[Categoria C],[Categoria D],[Categoria E]]
+patrons_temporals_avancats = [[],[],[],[],[]] #patrons_temporals_avançats > jugador > pics_max, pics_min
 desviacio_estandard = 0
 puntuacio_maxima = 0
 puntuacio_minima = 0
@@ -57,43 +57,43 @@ if puntuacions != [[],[],[],[],[]]:
     resta_mitjana_desviacioe = mitjana - desviacio_estandard
     for mitja in range(len(mitjanes_jugadors)):
         if mitjanes_jugadors[mitja] > suma_mitjana_desviacioe:
-            categories["Categories A"].append(f"Jugador{mitja + 1}")
+            categories["Categoria A"].append(mitja + 1)
         elif mitjanes_jugadors[mitja] < suma_mitjana_desviacioe and mitjanes_jugadors[mitja] > resta_mitjana_desviacioe:
-            categories["Categories B"].append(f"Jugador{mitja + 1}")
+            categories["Categoria B"].append(mitja + 1)
         elif mitjanes_jugadors[mitja] < resta_mitjana_desviacioe:
-            categories["Categories C"].append(f"Jugador{mitja + 1}")
+            categories["Categoria C"].append(mitja + 1)
 
     #Treiem la màxima puntuacío (Categoria D) y la minima (Categoria E):
     puntuacio_maxima = 0
     puntuacio_minima = 0
-    aux_categories_3 = ""
-    aux_categories_4 = ""
+    aux_categories_3 = 0
+    aux_categories_4 = 0
     for jugadors in range(len(puntuacions)):
         for tirada in puntuacions[jugadors]:
             if tirada > puntuacio_maxima:
-                aux_categories_3 = f"Jugador{jugadors + 1}"
+                aux_categories_3 = jugador + 1
                 puntuacio_maxima = tirada
-                categories["Categories D"] = f"Jugador{jugadors + 1}"
+                categories["Categoria D"] = jugador + 1
             elif tirada == puntuacio_maxima:
-                if f"Jugador{jugadors + 1}" != aux_categories_3:
-                    categories["Categories D"] = categories["Categories D"]+", "+f"Jugador{jugadors + 1}"
-                    aux_categories_3 = f"Jugador{jugadors + 1}"
+                if (jugador + 1) != aux_categories_3:
+                    categories["Categoria D"] = categories["Categoria D"]+", "+(jugador + 1)
+                    aux_categories_3 = (jugador + 1)
             if tirada < puntuacio_minima or puntuacio_minima == 0:
-                aux_categories_4 = f"Jugador{jugadors + 1}"
+                aux_categories_4 = jugador + 1
                 puntuacio_minima = tirada
-                categories["Categories E"] = f"Jugador{jugadors + 1}"
+                categories["Categoria E"] = jugador + 1
             elif tirada == puntuacio_minima:
-                if f"Jugador{jugadors + 1}" != aux_categories_4:
-                    categories["Categories E"] = categories["Categories E"]+", "+f"Jugador{jugadors + 1}"
-                    aux_categories_4 = f"Jugador{jugadors + 1}"
+                if (jugador + 1) != aux_categories_4:
+                    categories["Categoria E"] = categories["Categoria E"]+", "+(jugador + 1)
+                    aux_categories_4 = jugador + 1
             
 
     print(f"""\nCATEGORIES:
-    A: {categories["Categories A"]}
-    B: {categories["Categories B"]}
-    C: {categories["Categories C"]}
-    D: {categories["Categories D"]}
-    E: {categories["Categories E"]}
+    A: {categories["Categoria A"]}
+    B: {categories["Categoria B"]}
+    C: {categories["Categoria C"]}
+    D: {categories["Categoria D"]}
+    E: {categories["Categoria E"]}
     """)       
 
     #Programa per els patrons temporals avançats:
@@ -110,23 +110,24 @@ if puntuacions != [[],[],[],[],[]]:
 
             if len(puntuacions[jugador]) > 2:
                 if puntuacions[jugador][tirada] < puntuacions[jugador][posicio_x1] and puntuacions[jugador][posicio_x1] > puntuacions[jugador][posicio_x2]:
-                    patrons_temporals_avancats[jugador][0].append(f"{tirada}, {posicio_x2}")
+                    tupla = (tirada, posicio_x2)
+                    patrons_temporals_avancats[jugador].append(tupla)
             
             if len(puntuacions[jugador]) > 2:
                 if puntuacions[jugador][tirada] > puntuacions[jugador][posicio_x1] and puntuacions[jugador][posicio_x1] < puntuacions[jugador][posicio_x2]:
-                    patrons_temporals_avancats[jugador][1].append(f"{tirada}, {posicio_x2}")
+                    tupla = (tirada, posicio_x2)
+                    patrons_temporals_avancats[jugador].append(tupla)
 
 
     for jugador in range(len(patrons_temporals_avancats)):
         print(f"""\nPATRO JUGADOR {jugador + 1}
-    Pics de pujada: {patrons_temporals_avancats[jugador][0]}
-    Pics de baixada: {patrons_temporals_avancats[jugador][1]} """)
+    Patrons Temportals Jugador{jugador} {patrons_temporals_avancats[jugador]}""")
 
     #Programes per patrons evidents:
     print("\nPATRONS EVIDENTS: ")
     for jugadors in range(len(puntuacions)):
-        decreix = 0
-        creix = 0
+        patro_decreixent = 0
+        patro_crecent = 0
         posicio_x1 = 0
         for tirada in range(len(puntuacions[jugadors])):
             if tirada < (len(puntuacions[jugadors]) - 1):
@@ -134,15 +135,15 @@ if puntuacions != [[],[],[],[],[]]:
             else:
                 posicio_x1 = tirada
             if puntuacions[jugadors][tirada] > puntuacions[jugadors][posicio_x1]:
-                decreix += 1
+                patro_decreixent += 1
             elif puntuacions[jugadors][tirada] < puntuacions[jugadors][posicio_x1]:
-                creix += 1
-        total = creix + decreix
-        if creix != 0 and ((creix/total)*100) >= 70:
-            print(f"Jugador{jugadors + 1}: Patro evident de creixement")
-        elif decreix != 0 and ((decreix/total)*100) >= 70:
-            print(f"Jugador{jugadors + 1}: Patro evident de decreixement")
+                patro_crecent += 1
+        total = patro_crecent + patro_decreixent
+        if patro_crecent != 0 and ((patro_crecent/total)*100) >= 70:
+            print(f"Jugador{jugadors + 1}: Patro evident de creixement\n")
+        elif patro_decreixent != 0 and ((patro_decreixent/total)*100) >= 70:
+            print(f"Jugador{jugadors + 1}: Patro evident de decreixement\n")
         else:
-            print(f"Jugador{jugadors + 1}: No hi ha patró evident")
+            print(f"Jugador{jugadors + 1}: No hi ha patró evident\n")
 else:
     print("NO HAY TIRADAS")
