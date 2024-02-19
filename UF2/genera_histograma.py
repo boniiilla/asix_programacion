551442332acd47008de07d845667e984602df7c8
boniiilla <c.bonillac00@gmail.com>
2024-02-19 13:11:17 +0100

    Codigo completo

---
+++ UF2/genera_histograma.py
@@ -0,0 +1,45 @@
+def posibles_resultados():
+    resultados = []
+    for dado1 in range(1,7):
+        for dado2 in range(1,7):
+            resultado = dado1 + dado2
+            resultados.append(resultado)
+    return resultados
+
+def sumar_valores(resultados):
+    resultados = posibles_resultados()
+    valores_resultados = ["","","","","","","","","","","",""]
+    for valor in range(2,13):
+        for resultado in resultados:
+            if resultado == valor:
+                valores_resultados[valor - 2] += "*"
+    return valores_resultados
+
+def max_valor(valores_resulados):
+    max_numero = 0
+    for valor1 in range(len(valores_resulados)):
+        for valor2 in range(valor1, len(valores_resulados)):
+            if len(valores_resulados[valor1]) < len(valores_resulados[valor2]):
+                max_numero = valor2 + 2
+    return max_numero
+
+
+def printear_valores():
+    resultados = posibles_resultados()
+    valores_resultados = sumar_valores(resultados)
+    max_numero = max_valor(valores_resultados)
+    print(f"""2: {valores_resultados[0]}
+3: {valores_resultados[1]}
+4: {valores_resultados[2]}
+5: {valores_resultados[3]}
+6: {valores_resultados[4]}
+7: {valores_resultados[5]}
+8: {valores_resultados[6]}
+9: {valores_resultados[7]}
+10: {valores_resultados[8]}
+11: {valores_resultados[9]}
+12: {valores_resultados[10]}
+El maxim és: {max_numero}""")
+
+
+printear_valores()
