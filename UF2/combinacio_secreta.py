339c0882d2624dad03cabeea7eb345bfead2f28e
boniiilla <c.bonillac00@gmail.com>
2024-02-23 09:30:44 +0100

    mejorado

--- UF2/combinacio_secreta.py
+++ UF2/combinacio_secreta.py
@@ -12,25 +12,30 @@ def palabra_secreta_aleatoria():
     return palabra_secreta
 
 def intentos_usuario(palabra_secreta):
-    palabra_usuario = 0
-    intento_usuario = "-----"
-    while intentos_usuario != "OOOOO":
+    intento_usuario = ""
+    while intento_usuario != "OOOOO":
+        aux_intento_usuario = ["-","-","-","-","-"]
+        intento_usuario = ""
         palabra_usuario = input(f"Escriu {len(palabra_secreta)} lletres minúscules: ")
         if len(palabra_usuario) == 5:
             for letra in range(5):
                 if palabra_usuario[letra] == palabra_secreta[letra]:
-                    intento_usuario = intento_usuario.replace(intento_usuario[letra], "O")
+                    aux_intento_usuario[letra] = "O"
                 for letra_usuario in range(5):
                     if palabra_secreta[letra] == palabra_usuario[letra_usuario] and letra != letra_usuario:
-                        intento_usuario = intento_usuario.replace(intento_usuario[letra], "X")
+                        aux_intento_usuario[letra] = "X"
+            for valor in aux_intento_usuario:
+                intento_usuario += valor
         else:
             print("Mida de paraula no valida.")
-        print(f"La resposta es [{intento_usuario}]. Continua intentant-ho!")
+        if intento_usuario != "OOOOO":
+            print(f"La resposta es [{intento_usuario}]. Continua intentant-ho!")
+        else:
+            print(f"La resposta es [{intento_usuario}]. Has encertat!")
 
 def endevina_combinacio():
     palabra_secreta = palabra_secreta_aleatoria()
     print(palabra_secreta)
     intentos_usuario(palabra_secreta)
-
 
 endevina_combinacio()
