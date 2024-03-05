73f2d2becafd7f720565056619d071cb9980fcbe
boniiilla <c.bonillac00@gmail.com>
2024-03-05 08:30:08 +0100

    agencia

---
+++ UF2/kotlin/agencia_viajes.kt
@@ -0,0 +1,10 @@
+fun lista_viajes(): List<String> {
+    val listadeviajes: List<String?> = listOf()
+
+    return listadeviajes
+}
+
+fun main() {
+    val listadeviajes: List<String?> = lista_viajes()
+    println(listadeviajes)
+} 
