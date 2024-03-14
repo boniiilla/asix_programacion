a1f70d9aa20760288522fe909372a2ef57d4a708
boniiilla <c.bonillac00@gmail.com>
2024-03-14 08:23:51 +0100

    Ejercicio insertado

---
+++ UF2/modularitat_classes.py
@@ -0,0 +1,30 @@
+# Coordenada inicial
+coordenada = (0, 0)
+
+# Programa principal
+def moure_dreta(coordenada):
+    x, y = coordenada
+    nova_coordenada = (x + 1, y)
+    return nova_coordenada
+
+def moure_esquerra(coordenada):
+    x, y = coordenada
+    nova_coordenada = (x - 1, y)
+    return nova_coordenada
+
+def moure_amunt(coordenada):
+    x, y = coordenada
+    nova_coordenada = (x, y + 1)
+    return nova_coordenada
+
+def moure_avall(coordenada):
+    x, y = coordenada
+    nova_coordenada = (x, y - 1)
+    return nova_coordenada
+
+# Executar moviments
+coordenada = moure_dreta(coordenada)
+print(f"Nova coordenada després de moure a la dreta: {coordenada}")
+
+coordenada = moure_amunt(coordenada)
+print(f"Nova coordenada després de moure amunt: {coordenada}")
