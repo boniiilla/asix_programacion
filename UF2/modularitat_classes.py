c8c3b791e7982fa2e3b107ae04e16a57f116eaf6
boniiilla <c.bonillac00@gmail.com>
2024-03-14 08:40:59 +0100

    Nada

--- UF2/modularitat_classes.py
+++ UF2/modularitat_classes.py
@@ -3,9 +3,9 @@ coordenada = (0, 0)
 
 # Programa principal
 def moure_dreta(coordenada):
-    x, y = coordenada
-    nova_coordenada = (x + 1, y)
-    return nova_coordenada
+        x, y = coordenada
+        nova_coordenada = (x + 1, y)
+        return nova_coordenada
 
 def moure_esquerra(coordenada):
     x, y = coordenada
