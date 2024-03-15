37f6ed8d95753eff846a51a1dc5b9cb874f96e37
boniiilla <c.bonillac00@gmail.com>
2024-03-15 09:03:57 +0100

    Añadir libro en kotlin

--- UF2/kotlin/biblioteca/biblioteca.kt
+++ UF2/kotlin/biblioteca/biblioteca.kt
@@ -1,5 +1,24 @@
 class Llibre {
+    var libros: MutableMap<String, String>
+    var libro: String
+    var autor: String
 
+    constructor() {
+        this.libros = mutableMapOf(
+            "Harry Potter 1" to "J.K. Rowling"
+        )
+        this.libro = ""
+        this.autor = ""
+    }
+
+    fun anadir_libro() {
+        println("Introduce el libro que quieres añadir:")
+        this.libro = readLine()!!.toString()
+        println("Introduce el autor del libro:")
+        this.autor = readLine()!!.toString()
+        
+        this.libros.put(this.libro, this.autor)
+    }
 }
 
 class Soci {
@@ -12,8 +31,13 @@ class Prestec {
 
 class Main {
 
+    constructor() {
+        var llibre: Llibre = Llibre()
+        llibre.anadir_libro()
+        println("Estos son los libros que hay ${llibre.libros}")
+    }
 }
 
 fun main() {
-    
+    Main()
 }
