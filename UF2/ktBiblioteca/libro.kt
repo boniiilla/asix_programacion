0f1ef0c5544571dc722ca25def3513779e309259
boniiilla <c.bonillac00@gmail.com>
2024-03-22 08:54:28 +0100

    no compila libro

--- UF2/ktBiblioteca/libro.kt
+++ UF2/ktBiblioteca/libro.kt
@@ -1,10 +1,10 @@
 package Biblioteca
 
-class Libro(val titulo: String, val autor: String, var ejemplaresDisponibles: Int) {
+class Libro(val titulo: String, val autor: String, var cantidad: Int) {
 
     fun prestar() {
-        if (ejemplaresDisponibles > 0) {
-            ejemplaresDisponibles--
+        if (cantidad > 0) {
+            cantidad--
             println("Libro prestado: $titulo, Autor: $autor")
         } else {
             println("No hay ejemplares disponibles de $titulo")
@@ -12,11 +12,11 @@ class Libro(val titulo: String, val autor: String, var ejemplaresDisponibles: In
     }
 
     fun devolver() {
-        ejemplaresDisponibles++
+        cantidad++
         println("Libro devuelto: $titulo, Autor: $autor")
     }
 
     fun informacion() {
-        println("Título: $titulo, Autor: $autor, Ejemplares Disponibles: $ejemplaresDisponibles")
+        println("Título: $titulo, Autor: $autor, Ejemplares Disponibles: $cantidad")
     }
 }
