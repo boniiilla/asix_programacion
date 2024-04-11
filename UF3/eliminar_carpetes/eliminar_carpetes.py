9437ce33a965997136ea21c8ef2288187e701ada
boniiilla <c.bonillac00@gmail.com>
2024-04-11 08:31:21 +0200

    Nada

--- UF3/eliminar_carpetes/eliminar_carpetes.py
+++ UF3/eliminar_carpetes/eliminar_carpetes.py
@@ -1,13 +1,32 @@
 import os
 
 '''
+Bonilla:
+
 1. Eliminar carpeta
 2.1. Eliminar el contenido
 2.2. Eliminar la carpeta
 3.1.1. Pedir al usuario la ruta de la carpeta
 3.1.2. Comprobar la existencia de la carpeta
 3.1.3. Ver si hay subcarpetas
-4.3.3.1. Eliminar el contenido de la subcarpeta
+4.1.1.1. Eliminar el contenido de la subcarpeta
+
+Albert:
+
+1. Eliminar carpeta
+2.1. Obtener ruta
+2.2. ELiminar Carpeta
+3.1.1. Pedir al usuario la ruta a eliminar
+3.2.1. Verificar que existe la ruta
+3.2.2. Verificar que es una carpeta
+3.2.3. Eliminar contenido
+3.2.4. Eliminar carpeta
+4.1.1.1. Pedir al usuario la ruta a eliminar
+4.1.1.1. Verificar que existe la ruta
+4.1.2.1. Verificar que es una carpeta
+4.1.3.1. Mirar contenido
+4.1.3.2. Eliminar ficheros
+4.1.3.3. Iterar en las carpetas para elimnar las carpetas
 '''
 
 class Eliminar:
