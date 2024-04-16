b319f1d1dca3545d319a99b2df59fd27d9b446b8
boniiilla <c.bonillac00@gmail.com>
2024-04-16 11:38:42 +0200

    First java code

---
+++ hello_world.java
@@ -0,0 +1,14 @@
+class HelloWorld
+{
+    /**
+     * Una funcion se declara poniendo:
+     * Su visibilidad
+     * Si es static o no (si no es static no se pone nada)
+     * El tipo de dato que retorna, si no devuelve nada void
+     * Nombre del metodo
+     * Parametros del metodo
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
