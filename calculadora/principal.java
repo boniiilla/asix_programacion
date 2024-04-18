775154d6accfea69d9fda7b532fe453ab41594db
boniiilla <c.bonillac00@gmail.com>
2024-04-18 10:06:37 +0200

    Codigo calculadora

--- calculadora/principal.java
+++ calculadora/principal.java
@@ -2,6 +2,18 @@ package calculadora;
 
 class Principal {
     public static void main(String[] args) {
-        Pantalla.escribir("Hola Mundo");
+        String[] opciones = {
+            "1 - Sumar",
+            "2 - Restar",
+            "3 - Multiplicar",
+            "4 - Dividir",
+            "5 - Ecuacion de segundo grado"
+        };
+
+        for (String opcion :opciones) {
+            Pantalla.escribir(opcion);
+        };
+
+        String opcion = Teclado.leer(opciones);
     }
 }
