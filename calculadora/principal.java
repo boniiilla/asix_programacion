cd25967923f59e3d1d159ce807fb493e5d00773a
boniiilla <c.bonillac00@gmail.com>
2024-04-18 09:21:43 +0200

    Errores y compilacion en java

---
+++ calculadora/principal.java
@@ -0,0 +1,7 @@
+package calculadora;
+
+class principal {
+    public static void main(String[] args) {
+        Pantalla.Escribir("Hola Mundo");
+    }
+}
--- calculadora/teclado.java
+++ calculadora/teclado.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 class teclado {
     public static String leer(String texto) {
         Scanner input = new Scanner(System.in);
-        Pantalla pantalla = new Pantalla();
-        pantalla.escribir(texto);
+        Pantalla.Escribir(texto);
 
         String result = input.nextLine();
 
