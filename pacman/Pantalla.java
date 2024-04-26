9c2b115f7930221f41fa03dd2f79ac3d151394c8
boniiilla <c.bonillac00@gmail.com>
2024-04-26 08:22:46 +0200

    class Pantalla y Class Teclado extraidos por si acaso

---
+++ pacman/Pantalla.java
@@ -0,0 +1,8 @@
+package pacman;
+
+class Pantalla
+{
+    public static void escribir(String texto){
+        System.out.println(texto);
+    }
+}
---
+++ pacman/Teclado.java
@@ -0,0 +1,14 @@
+package pacman;
+
+import java.util.Scanner;
+
+class Teclado {
+    public static String leer(String texto) {
+        Scanner input = new Scanner(System.in);
+        Pantalla.escribir(texto);
+
+        String result = input.nextLine();
+
+        return result;
+    }
+}
