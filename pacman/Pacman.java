daf236cca180c67f2b5abca4ea9fc464ae3adfb7
boniiilla <c.bonillac00@gmail.com>
2024-04-26 09:03:53 +0200

    Getters

--- pacman/Pacman.java
+++ pacman/Pacman.java
@@ -1,5 +1,30 @@
 package pacman;
 
 class Pacman {
+    private int newposX; 
+    private int newposY;
+    private int puntuacio;
+    private int lifes;
+
+    public Pacman(int newposX, int newposY, int puntuacio, int lifes) {
+        this.newposX = newposX;
+        this.newposY = newposY;
+        this.puntuacio = puntuacio;
+        this.lifes = lifes;
+    }
+    /** Getters **/
+    public int getnewPosX(){
+        return this.newposX;
+    }
+    public int getnewPosY(){
+        return this.newposY;
+    }
+    public int getpuntutacio(){
+        return this.puntuacio;
+    }
+    public int getLifes(){
+        return this.lifes;
+    }
+
     
 }
