1258d9de68a82cc13671ea8639a35f1b942f8713
boniiilla <c.bonillac00@gmail.com>
2024-04-26 08:44:46 +0200

    codigo main empezado

--- pacman/Main.java
+++ pacman/Main.java
@@ -1,5 +1,10 @@
 package pacman;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Pacman pacman = new pacman.Pacman();
+        pacman.move(0, 0);
+        
+    }
+
 }
