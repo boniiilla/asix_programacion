63e3f5efbf098abc637437b959f858df6a2de894
boniiilla <c.bonillac00@gmail.com>
2024-05-02 09:12:33 +0200

    Registrar notas por teclado

--- notes_alumnes/GestorNotes.java
+++ notes_alumnes/GestorNotes.java
@@ -1,11 +1,22 @@
 package notes_alumnes;
 
+import java.util.Scanner;
 
 class GestorNotes {
-    private String nomAlumne;
-    private double[] notes;
 
     public void registrarNotes(String nomAlumne, double[] notes){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Introduce el numero de notas para registrar: ");
+        int num_notes = input.nextInt();
+
+        notes = new double[num_notes];
+
+        System.out.println("Introduce las notas del alumno: ");
+        for (int i = 0; i < num_notes; i++) {
+            System.out.println("Nota" + i + 1 + ":");
+            notes[i] = input.nextDouble();
+        }
         
     }
 
--- notes_alumnes/Main.java
+++ notes_alumnes/Main.java
@@ -2,6 +2,16 @@ package notes_alumnes;
 
 class Main {
     public static void main(String[] args) {
+        GestorNotes gestornotes = new GestorNotes();
+        CalculadoraEstadistiques calcular = new CalculadoraEstadistiques();
         
+        String nombre = "Carlos Bonilla";
+        double[] notes = {8.50, 7.00, 9.67};
+
+        gestornotes.registrarNotes(nombre, notes);
+
+        System.out.println(gestornotes.obtenirNotes(nombre));
+
+
     }
 }
