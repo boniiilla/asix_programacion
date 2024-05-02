1518fecb321a0e18f7dd84093fc0d04e88d2ae8d
boniiilla <c.bonillac00@gmail.com>
2024-05-02 09:22:02 +0200

    gestro notas corregido

--- notes_alumnes/GestorNotes.java
+++ notes_alumnes/GestorNotes.java
@@ -1,26 +1,17 @@
 package notes_alumnes;
 
+import java.util.Dictionary;
 import java.util.Scanner;
 
-class GestorNotes {
+public class GestorNotes {
+
+    Dictionary<String, double[]> alumnes = new Dictionary<String, double[]>();
 
     public void registrarNotes(String nomAlumne, double[] notes){
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Introduce el numero de notas para registrar: ");
-        int num_notes = input.nextInt();
-
-        notes = new double[num_notes];
-
-        System.out.println("Introduce las notas del alumno: ");
-        for (int i = 0; i < num_notes; i++) {
-            System.out.println("Nota" + i + 1 + ":");
-            notes[i] = input.nextDouble();
-        }
-        
+        alumnes.put(nomAlumne, notes);
     }
 
     public double[] obtenirNotes(String nomAlumne) {
-        return notes;
+        return alumnes.get(nomAlumne);
     }
 }
