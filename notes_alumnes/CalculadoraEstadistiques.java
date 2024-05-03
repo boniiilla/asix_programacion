52230b17a0e84f3e9a68620bec213fe46f42bf32
boniiilla <c.bonillac00@gmail.com>
2024-05-03 08:31:34 +0200

    CalculadoraEstadistiques profe

--- notes_alumnes/CalculadoraEstadistiques.java
+++ notes_alumnes/CalculadoraEstadistiques.java
@@ -1,7 +1,10 @@
 package notes_alumnes;
 
-class CalculadoraEstadistiques {
-    public static double calcularMitjana(double[] notes){
+import java.util.Arrays;
+
+public class CalculadoraEstadistiques
+{
+    public static double calcularMitjana(double[] notes) {
         double suma = 0.0;
 
         for (double nota : notes) {
@@ -12,12 +15,14 @@ class CalculadoraEstadistiques {
     }
 
     public static double calcularMaxim(double[] notes) {
-        Array.sort(notes);
+        Arrays.sort(notes);
+
         return notes[notes.length - 1];
     }
 
     public static double calcularMinim(double[] notes) {
-        Array.sort(notes);
+        Arrays.sort(notes);
+
         return notes[0];
     }
 }
