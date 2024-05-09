def4116ff3fbdc61056acc63a12a11b7fb12d137
boniiilla <c.bonillac00@gmail.com>
2024-05-09 08:09:31 +0200

    afegir tasca acabado

--- gestor_tasques/GestorTasques.java
+++ gestor_tasques/GestorTasques.java
@@ -1,16 +1,17 @@
 package gestor_tasques;
 
 import java.util.HashMap;
-import java.util.Scanner;
 
 public class GestorTasques {
-    private static Scanner sc = new Scanner(System.in);
 
     HashMap<String, HashMap<String, String>> tasques = new HashMap<String, HashMap<String, String>>();
 
-    public void afegirTasca(String tasca, HashMap<String, String> prioritat_estat){
-        System.out.println("Pioritat de la tasca: ");
-        String prioritat = sc.next();
-        prioritat_estat.put(prioritat, "Pendent");
+    public void afegirTasca(String tasca, String prioritat, String estat){
+        HashMap<String, String> prioritat_estat = new HashMap<String, String>();
+
+        prioritat_estat.put(prioritat, estat);
+
+        tasques.put(tasca, prioritat_estat);
+        
     }
 }
