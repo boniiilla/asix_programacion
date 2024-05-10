e02938c5f77b8e6d262c2ad1c0691a65fcc874e1
boniiilla <c.bonillac00@gmail.com>
2024-05-10 09:06:29 +0200

    getNom i getPersonatge

--- GestioAventura/Personatges.java
+++ GestioAventura/Personatges.java
@@ -5,8 +5,30 @@ class Personatges {
     private int nivell;
     private int puntsMana;
     private int puntsVida;
-    private String Arma;
-    
+    private String arma;
+    private String armadura;
+
+    public Personatges(String nom, int nivell, int puntsVida, int puntsMana, String arma, String armadura){
+        this.nom = nom;
+        this.nivell = nivell;
+        this.puntsVida = puntsVida;
+        this.puntsMana = puntsMana;
+        this.arma = arma;
+        this.armadura = armadura;
+    }
+
+    public String getNom(){
+        return this.nom;
+    }
+
+    public String getPersonatge() {
+        return "Nom:" + this.nom +
+        "Nivell: " + this.nivell +
+        "Punts de vida: " + this.puntsVida +
+        "Punts de mana: " + this.puntsMana +
+        "Arma: " + this.arma + 
+        "Armadura: " + this.armadura;
+    }
 }
 
 class Main {
