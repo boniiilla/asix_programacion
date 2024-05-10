3797ff62e6bc53c4d703da015f66d6b26fe3adfc
boniiilla <c.bonillac00@gmail.com>
2024-05-10 09:52:42 +0200

    extraccion del doc XML

--- GestioAventura/Parser.java
+++ GestioAventura/Parser.java
@@ -6,13 +6,13 @@ import java.io.*;
 
 class Parser {
     public static void main(String[] args) throws Exception{
-        System.out.println("Llegir el XML:");
+        System.out.println("\nLlegir el XML:");
         readXMLPersonatges();
 
-        System.out.println("Gestió de elements del joc carregat:");
+        System.out.println("\nGestió de elements del joc carregat: ");
         ElementsManager();
 
-        System.err.println("Càrrega de caracteritiques dels personatges:");
+        System.err.println("\nCàrrega de caracteritiques dels personatges: ");
         CharacteristicLoad();
     }
 
@@ -30,14 +30,57 @@ class Parser {
             for (int i = 0; i < l.getLength(); i++) {
                 nPersonatges++;
             }
-            System.out.println("Personatges del joc: " + nPersonatges);
+            System.out.println("\tPersonatges del joc: " + nPersonatges);
         } catch (Exception ex) {
             System.out.println("Error en la lectura del document: " + ex);
             throw ex;
         }
     }
 
-    private static void ElementsManager() throws Exception {}
+    private static void ElementsManager() throws Exception {
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        Document document = null;
+
+        try {
+            DocumentBuilder builder = builderFactory.newDocumentBuilder();
+            File f = new File ("GestioAventura/personatges.xml");
+            document = builder.parse(f);
+            Element e = document.getDocumentElement();
+
+            NodeList llistaElems = e.getElementsByTagName("nom");
+
+            for (int i = 0; i < llistaElems.getLength(); i++) {
+                Element elem = (Element)llistaElems.item(i);
+                NodeList llistaFills = elem.getChildNodes();
+                String text = "";
+                for (int j = 0; j < llistaFills.getLength(); j++) {
+                    Node n = llistaFills.item(j);
+                    if ( (n.getNodeType() == Node.TEXT_NODE)||
+                        (n.getNodeType() == Node.CDATA_SECTION_NODE) ) {
+                
+                        text += n.getNodeValue();
+                    }
+                }
+
+                NodeList llistaNivell = ((Element)elem.getParentNode()).getElementsByTagName("nivell");
+                for (int j = 0; j < llistaNivell.getLength(); j++) {
+                    Element preu = (Element)llistaNivell.item(j);
+                    NodeList llistaFillsNivell = preu.getChildNodes();
+                    for (int k = 0; k < llistaFillsNivell.getLength(); k++) { 
+                        Node p = llistaFillsNivell.item(k);
+                        if ( (p.getNodeType() == Node.TEXT_NODE)||
+                            (p.getNodeType() == Node.CDATA_SECTION_NODE) ) {
+                    
+                            text += " " + p.getNodeValue();
+                        }
+                    }
+                    System.out.println("\t" + text);
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println("Error en la lectura del document: " + ex);
+        }
+    }
 
     private static void CharacteristicLoad() throws Exception {}
 
