c7138ccbabf0eae746942cced359e5522c30cbac
boniiilla <c.bonillac00@gmail.com>
2024-05-10 08:25:39 +0200

    class Parser

---
+++ GestioAventura/Parser.java
@@ -0,0 +1,11 @@
+package GestioAventura;
+
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import java.io.*;
+
+public class Parser {
+    public static void main(String[] args) {
+        
+    }
+}
