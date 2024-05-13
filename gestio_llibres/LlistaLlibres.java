bc94b1a2912deb778ea99699a4a1deb12d56af98
boniiilla <c.bonillac00@gmail.com>
2024-05-13 09:57:46 +0200

    addBooks i printAllBooks

--- gestio_llibres/LlistaLlibres.java
+++ gestio_llibres/LlistaLlibres.java
@@ -1,6 +1,7 @@
 package gestio_llibres;
 
 import java.util.HashMap;
+import java.util.ArrayList;
 
 class Novela {
     private String title;
@@ -36,23 +37,58 @@ class Poesia {
 }
 
 class LlistaLlibres<T> {
-    private HashMap<T, String[]> books;
+    private HashMap<T, ArrayList<Book>> books;
 
     public LlistaLlibres(){
-        books = new HashMap<T, String[]>();
+        books = new HashMap<T, ArrayList<Book>>();
+        books.put((T) "Poesia", new ArrayList<Book>());
+        books.put((T)"Novela", new ArrayList<Book>());
+        books.put((T)"Ciencia-Ficcio", new ArrayList<Book>());
     }
 
-    public void addBooks(T genere, String title){}
+    public void addBooks(T genere, String title){
+        Book libro = new Book(title);
+        books.get(genere).add(libro);
+    }
 
     public void deleteBook(){}
 
     public void searchBook(String title){}
 
-    public void printAllBooks(){}
+    public void printAllBooks(T genere){
+        System.out.println("\nLlibres de genere: " + genere);
+        for (Book llibre: books.get(genere)) {
+            System.out.println(llibre);
+        }
+    }
+}
+
+class Book {
+    private String titol;
+
+    public Book(String titol) {
+        this.titol = titol;
+    }
+
+    public String getTitol(){
+        return this.titol;
+    }
+
+    public String toString(){
+        return this.titol;
+    }
 }
 
 class Main {
-    public static void main(String[] args) {
-        
+    public static <T> void main(String[] args) {
+        LlistaLlibres<T> biblioteca = new LlistaLlibres<T>();
+
+        System.out.println("Add book");
+        biblioteca.addBooks((T)"Ciencia-Ficcio", "Harry Potter");
+        System.out.println("Totes els llibres: ");
+        biblioteca.printAllBooks((T)"Ciencia-Ficcio");
+        biblioteca.printAllBooks((T)"Novela");
+        biblioteca.printAllBooks((T)"Poesia");
+
     }
 }
