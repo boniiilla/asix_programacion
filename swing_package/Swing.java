6f0c6b24e347ae1bcf6bd449ff250fb07b4e017e
boniiilla <c.bonillac00@gmail.com>
2024-05-16 09:03:49 +0200

    managerButton i managerGeneral

--- swing_package/Swing.java
+++ swing_package/Swing.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.event.*;
 
@@ -13,17 +14,20 @@ class Swing {
         JFrame frame = new JFrame("CALUCLADORA");
         JTextField pantallita = new JTextField(); 
         JButton[] numericButtons = new JButton[10];
+        GridLayout managerButton = new GridLayout(4, 3);
+        BoxLayout managerGeneral = new BoxLayout(frame, BoxLayout.Y_AXIS);
 
         for (int i = 0; i < numericButtons.length; i++) {
             numericButtons[i] = new JButton(Integer.toString(i));
         }
-
-        pantallita.setEditable(false);
         
-        frame.add(pantallita);
+        pantallita.setEditable(false);
+
         for (JButton jButton : numericButtons) {
-            frame.add(jButton);
+            managerButton.addLayoutComponent(jButton.getName(), jButton);
         }
+        frame.add(pantallita);
+        
         frame.setSize(300, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
