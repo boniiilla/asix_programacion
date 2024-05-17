6ddae735c7094fa19c99e431f1d98741eba5c52d
boniiilla <c.bonillac00@gmail.com>
2024-05-17 08:48:50 +0200

    prints per veure si esta ben fet

--- tasques_profe/TaskManagement.java
+++ tasques_profe/TaskManagement.java
@@ -1,5 +1,6 @@
 package tasques_profe;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -15,15 +16,19 @@ class TaskManagement {
         lists.put("Completada", new ArrayList<Task>());
     }
 
-    public void addTask(String taskName, String estat) {
-        Task task = new Task(taskName);
-        lists.get(estat).add(task);
+    public void addTask(String taskName, String description, String date) {
+        Task task = new Task(taskName, description, date);
         tasks.put(task.getId(), task);
     }
 
     public void doneTask(int taskId) {
         Task task = tasks.get(taskId);
         task.done();
+    }
+
+    public void onGoingTask(int taskid) {
+        Task task = tasks.get(taskid);
+        task.onGoing();
     }
 
     public void deleteTask(int taskId) {
@@ -48,16 +53,24 @@ class Task {
     private static int numInstances = 0;
     private int id;
     private String name;
+    private String endDate;
+    private String description;
     private String status;
 
-    public Task(String name){
+    public Task(String name, String description, String endDate){
         this.name = name;
+        this.description = description;
+        this.endDate = endDate;
         status = "Pendent";
         id = ++numInstances;
     }
 
     public int getId() {
         return id;
+    }
+
+    public void modifyTask() {
+
     }
 
     public void onGoing(){
@@ -69,7 +82,13 @@ class Task {
     }
 
     public String toString() {
-        return "Id: " + id + " Name: " + name + " Status: " + status;
+        return 
+        "===========================================" +
+        "\nId: " + id + 
+        "\nName: " + name +
+        "\nDescription: " + description +
+        "\nEnd date: " + endDate +
+        "\nStatus: " + status;
     }
 }
 
@@ -79,18 +98,16 @@ class Main {
 
         TaskManagement manager = new TaskManagement();
 
-        System.out.println("Add task:");
-        manager.addTask("Hacer ejercicio", "Media");
+        System.out.println("Add new task:");
+        manager.addTask("Hacer ejercicio", "Ejercicios para fortalecer el abdomen", "25/05/24");
         System.out.println("Print all tasks:");
         manager.printAllTask();
-        System.out.println("Mark as done");
-        manager.doneTask(1);
+        System.out.println("Add new task:");
+        manager.addTask("Jugar a futbol", "Realizar el deporte llamado futbol con amigos en una pista de futbol sala", "18/05/24");
         System.out.println("Print all tasks:");
         manager.printAllTask();
-        System.out.println("Print by priority");
-        manager.printTaskByPriority("Media");
-        System.out.println("Delete task");
-        manager.deleteTask(1);
+        System.out.println("Mark task on going:");
+        manager.onGoingTask(1);
         System.out.println("Print all tasks:");
         manager.printAllTask();
     }
