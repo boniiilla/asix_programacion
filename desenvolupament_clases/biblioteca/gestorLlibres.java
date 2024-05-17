cfa2f3ff4ce6ce1764d7dff40aeff94a00c2f36e
boniiilla <c.bonillac00@gmail.com>
2024-05-17 08:24:27 +0200

    Biblioteca

---
+++ desenvolupament_clases/biblioteca/gestorLlibres.java
@@ -0,0 +1,19 @@
+package desenvolupament_clases.biblioteca;
+
+import java.util.HashMap;
+
+public class gestorLlibres {
+    HashMap<Integer, Book> llibreria = new HashMap<Integer, Book>();
+
+    public void premerLlibres(){
+
+    }
+
+    public void devolverLlibres(){
+
+    }
+}
+
+class Book {
+
+}
