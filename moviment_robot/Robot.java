449b9b7c52e37e979ff2947433dec599ee2adfe3
boniiilla <c.bonillac00@gmail.com>
2024-05-24 08:22:46 +0200

    buttons

--- moviment_robot/Robot.java
+++ moviment_robot/Robot.java
@@ -5,5 +5,14 @@ import javax.swing.*;
 public class Robot {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Control de Robot");
+        JTextField pantallita = new JTextField();
+
+        JButton endavant = new JButton("Moure Endavant");
+        JButton enrere = new JButton("Moure Enrere");
+        JButton esquerra = new JButton("Girar Esquerra");
+        JButton save_stat = new JButton("Guardar Estat");
+        JButton load_stat = new JButton("Carregar Estat");
+
+        
     }
 }
