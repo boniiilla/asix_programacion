cd87a4afbdac33e8560b4b89fe5de41a5dae2fcf
boniiilla <c.bonillac00@gmail.com>
2024-05-24 08:17:33 +0200

    JFrame

---
+++ moviment_robot/Robot.java
@@ -0,0 +1,9 @@
+package moviment_robot;
+
+import javax.swing.*;
+
+public class Robot {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Control de Robot");
+    }
+}
