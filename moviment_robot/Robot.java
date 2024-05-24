e5d9fa4c127e936481c99b51fc9ce5b1d154b808
boniiilla <c.bonillac00@gmail.com>
2024-05-24 09:15:37 +0200

    x e y

--- moviment_robot/Robot.java
+++ moviment_robot/Robot.java
@@ -1,12 +1,37 @@
 package moviment_robot;
 
 import javax.swing.*;
+
+import java.awt.Dimension;
 import java.awt.GridLayout;
+
+class Coordenade {
+    private int x = 0;
+    private int y = 0;
+    private String direccio = "Nord";
+
+    public void sumX(int x) {
+        ++x;
+    }
+
+    public void sumY(int y) {
+        ++y;
+    }
+
+    public String getDireccio() {
+        return direccio;
+    }
+    public String getCoordenade(){
+        return "(" + x + ", " + y + ")";
+    }
+}
 
 public class Robot {
     public static void main(String[] args) {
+        Coordenade coordenada = new Coordenade();
+
         JFrame frame = new JFrame("Control de Robot");
-        JTextField pantallita = new JTextField();
+        JTextField pantallita = new JTextField("     Estat del robot: " + coordenada.getCoordenade() + " Direcció: " + coordenada.getDireccio() + "     ");
         JPanel buttonPanel = new JPanel();
 
         JButton endavant = new JButton("Moure Endavant");
@@ -16,21 +41,18 @@ public class Robot {
         JButton save_stat = new JButton("Guardar Estat");
         JButton load_stat = new JButton("Carregar Estat");
 
-        GridLayout managerButton = new GridLayout(3, 3);
-        BoxLayout managerGeneral = new BoxLayout(frame, BoxLayout.Y_AXIS);
+        pantallita.setEditable(false);
 
-        managerGeneral.layoutContainer(frame);
-
+        buttonPanel.add(pantallita);
         buttonPanel.add(endavant);
         buttonPanel.add(enrere);
         buttonPanel.add(esquerra);
         buttonPanel.add(dreta);
         buttonPanel.add(save_stat);
         buttonPanel.add(load_stat);
+        pantallita.setSize(100,300);
 
-        frame.add(pantallita);
         frame.add(buttonPanel);
-        frame.setLayout(managerButton);
 
         frame.setSize(375, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
