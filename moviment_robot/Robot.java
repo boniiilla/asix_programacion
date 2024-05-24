c52896ff89f4914fd15a2aefcaf3e62a0df7bb3c
boniiilla <c.bonillac00@gmail.com>
2024-05-24 09:26:07 +0200

    botones endavant y enrere

--- moviment_robot/Robot.java
+++ moviment_robot/Robot.java
@@ -4,18 +4,28 @@ import javax.swing.*;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 class Coordenade {
     private int x = 0;
     private int y = 0;
     private String direccio = "Nord";
 
-    public void sumX(int x) {
+    public void sumX() {
         ++x;
     }
 
-    public void sumY(int y) {
+    public void sumY() {
         ++y;
+    }
+
+    public void restX() {
+        --x;
+    }
+
+    public void restY() {
+        --y;
     }
 
     public String getDireccio() {
@@ -50,12 +60,41 @@ public class Robot {
         buttonPanel.add(dreta);
         buttonPanel.add(save_stat);
         buttonPanel.add(load_stat);
-        pantallita.setSize(100,300);
 
         frame.add(buttonPanel);
 
         frame.setSize(375, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
+        endavant.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (coordenada.getDireccio() == "Nord") {
+                    coordenada.sumY();
+                } else if (coordenada.getDireccio() == "Oest"){
+                    coordenada.restX();
+                } else if (coordenada.getDireccio() == "Sud"){
+                    coordenada.restY();
+                } else if (coordenada.getDireccio() == "Est"){
+                    coordenada.sumX();
+                }
+                pantallita.setText("     Estat del robot: " + coordenada.getCoordenade() + " Direcció: " + coordenada.getDireccio() + "     ");
+            }
+        });
+
+        enrere.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (coordenada.getDireccio() == "Nord") {
+                    coordenada.restY();
+                } else if (coordenada.getDireccio() == "Oest"){
+                    coordenada.sumX();
+                } else if (coordenada.getDireccio() == "Sud"){
+                    coordenada.sumY();
+                } else if (coordenada.getDireccio() == "Est"){
+                    coordenada.restX();
+                }
+                pantallita.setText("     Estat del robot: " + coordenada.getCoordenade() + " Direcció: " + coordenada.getDireccio() + "     ");
+            }
+        });
     }
 }
