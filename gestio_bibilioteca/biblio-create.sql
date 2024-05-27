b3dd18e515fd393ba63a36bac25af55f37928c2f
boniiilla <c.bonillac00@gmail.com>
2024-05-27 13:05:13 +0200

    inserts

--- gestio_bibilioteca/biblio-create.sql
+++ gestio_bibilioteca/biblio-create.sql
@@ -9,9 +9,17 @@ CREATE TABLE Autor (
 
 CREATE TABLE Llibre (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    titol VARCHAR(24),
+    titol VARCHAR(64),
     anyPublicacio DATE,
     autorId INT,
     FOREIGN KEY (autorId) REFERENCES Autor(id)
 );
+
+INSERT INTO Autor(nom, cognom) VALUES('Carlos', 'Bonilla');
+INSERT INTO Autor(nom, cognom) VALUES('Izan', 'Lozano');
+
+INSERT INTO Llibre(titol, anyPublicacio, autorId) 
+VALUES ('Harry Potter', '2012-04-12', 1)
+INSERT INTO Llibre(titol, anyPublicacio, autorId) 
+VALUES ('Los Pitufos', '2015-09-01', 2)
 
