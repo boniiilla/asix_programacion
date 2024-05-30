0cb279d3ec1c9cfb29349747baac7a4f06749b22
boniiilla <c.bonillac00@gmail.com>
2024-05-30 08:24:16 +0200

    database manager

---
+++ pr_database/model/DatabaseManager.java
@@ -0,0 +1,33 @@
+package pr_database.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
+public class DatabaseManager {
+    private Connection connection;
+
+    public void connect() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://192.168.133.3:3306/biblio", "root", "@MVM2016");
+            System.out.println("Connexio establerta.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                System.out.println("Connexio tancada");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
