c8a50f839410f9f10e3b4ceb063c4a340279c1e3
boniiilla <c.bonillac00@gmail.com>
2024-05-31 08:40:32 +0200

    frame

--- tetris/Main.java
+++ tetris/Main.java
@@ -1,7 +1,13 @@
 package tetris;
 
-public class Main {
+import javax.swing.JFrame;
+
+class Main {
     public static void main(String[] args) {
-        
+        JFrame frame = new JFrame("TETRIS");
+
+        frame.setSize(500, 700);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
