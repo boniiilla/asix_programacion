891435c9dd52b9998ea9065c29d417ee8bd186f1
boniiilla <c.bonillac00@gmail.com>
2024-05-31 09:08:13 +0200

    @override

--- tetris/Board.java
+++ tetris/Board.java
@@ -1,10 +1,17 @@
 package tetris;
 
 import javax.swing.JPanel;
+
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Board extends JPanel implements ActionListener{
     public Board(){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
         
     }
 }
