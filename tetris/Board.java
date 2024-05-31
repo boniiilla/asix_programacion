88e0090565e2f4a7ccece687f583c503dae3916a
boniiilla <c.bonillac00@gmail.com>
2024-05-31 09:14:49 +0200

    TRATRAMINOES

--- tetris/Board.java
+++ tetris/Board.java
@@ -8,9 +8,11 @@ import java.awt.event.ActionListener;
 public class Board extends JPanel implements ActionListener{
     private static int BOARD_WIDHT = 10;
     private static int BOARD_HEIGHT = 30;
+
+    private Tetraminoes[] board;
     
     public Board(){
-
+        board = new Tetraminoes[BOARD_HEIGHT * BOARD_WIDHT];
     }
 
     @Override
